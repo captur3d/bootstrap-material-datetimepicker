b63c0976fc32680b2a684ccbcb23d16515c4a6eb
Aswanth kumar <askumar@suyati.com>
2017-01-19 11:54:49 +0530

    date picker updated based on design

--- js/bootstrap-material-datetimepicker.js
+++ js/bootstrap-material-datetimepicker.js
@@ -202,35 +202,16 @@
                  this.template = '<div class="dtp hidden" id="' + this.name + '">' +
                          '<div class="dtp-content">' +
                          '<div class="dtp-date-view">' +
-                         '<header class="dtp-header">' +
-                         '<div class="dtp-actual-day">Lundi</div>' +
-                         '<div class="dtp-close"><a href="javascript:void(0);"><i class="material-icons">clear</i></a></div>' +
-                         '</header>' +
-                         '<div class="dtp-date hidden">' +
-                         '<div>' +
+                         '<div class="dtp-time hidden">' +
+                         '<div class="dtp-actual-maxtime">23:55</div>' +
+                         '</div>' +
+                         '<div class="dtp-date">' +
                          '<div class="left center p10">' +
                          '<a href="javascript:void(0);" class="dtp-select-month-before"><i class="material-icons">chevron_left</i></a>' +
                          '</div>' +
-                         '<div class="dtp-actual-month p80">MAR</div>' +
                          '<div class="right center p10">' +
                          '<a href="javascript:void(0);" class="dtp-select-month-after"><i class="material-icons">chevron_right</i></a>' +
                          '</div>' +
-                         '<div class="clearfix"></div>' +
-                         '</div>' +
-                         '<div class="dtp-actual-num">13</div>' +
-                         '<div>' +
-                         '<div class="left center p10">' +
-                         '<a href="javascript:void(0);" class="dtp-select-year-before"><i class="material-icons">chevron_left</i></a>' +
-                         '</div>' +
-                         '<div class="dtp-actual-year p80">2014</div>' +
-                         '<div class="right center p10">' +
-                         '<a href="javascript:void(0);" class="dtp-select-year-after"><i class="material-icons">chevron_right</i></a>' +
-                         '</div>' +
-                         '<div class="clearfix"></div>' +
-                         '</div>' +
-                         '</div>' +
-                         '<div class="dtp-time hidden">' +
-                         '<div class="dtp-actual-maxtime">23:55</div>' +
                          '</div>' +
                          '<div class="dtp-picker">' +
                          '<div class="dtp-picker-calendar"></div>' +
@@ -253,8 +234,8 @@
                          '<div class="dtp-buttons">' +
                          '<button class="dtp-btn-now btn btn-flat hidden">' + this.params.nowText + '</button>' +
                          '<button class="dtp-btn-clear btn btn-flat hidden">' + this.params.clearText + '</button>' +
-                         '<button class="dtp-btn-cancel btn btn-flat">' + this.params.cancelText + '</button>' +
-                         '<button class="dtp-btn-ok btn btn-flat">' + this.params.okText + '</button>' +
+                         '<button class="dtp-btn-cancel btn btn-default btn-flat">' + this.params.cancelText + '</button>' +
+                         '<button class="dtp-btn-ok btn btn-flat btn-primary">' + this.params.okText + '</button>' +
                          '<div class="clearfix"></div>' +
                          '</div>' +
                          '</div>' +
